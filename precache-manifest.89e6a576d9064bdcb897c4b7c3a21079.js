af419741543b662e027c64f82b0be443f524b43d
jungRoit <roit.github@gmail.com>
2021-02-02 00:30:15 +0545

    Updates

---
+++ precache-manifest.89e6a576d9064bdcb897c4b7c3a21079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7debf93f38e6664a58e11b720648b513",
+    "url": "/react-graphql-app/index.html"
+  },
+  {
+    "revision": "2d07fac25845e87c64ff",
+    "url": "/react-graphql-app/static/css/main.eae0fff2.chunk.css"
+  },
+  {
+    "revision": "f172f55f575ce162156a",
+    "url": "/react-graphql-app/static/js/2.69e230cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-graphql-app/static/js/2.69e230cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d07fac25845e87c64ff",
+    "url": "/react-graphql-app/static/js/main.f20a4cbd.chunk.js"
+  },
+  {
+    "revision": "7a36abd82c14a0d781eb",
+    "url": "/react-graphql-app/static/js/runtime-main.25d4155e.js"
+  },
+  {
+    "revision": "8f0a313e8e2c2165887a7efac033aeb0",
+    "url": "/react-graphql-app/static/media/search.8f0a313e.png"
+  }
+]);
